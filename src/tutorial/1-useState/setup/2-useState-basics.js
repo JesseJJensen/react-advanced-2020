3a8010cbc3c9484ea712455cf7b9b6a61f441be6
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-30 15:32:02 -0400

    useState basics => added logic to change title back after second click

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -8,8 +8,12 @@ const UseStateBasics = () => {
   const [text,setText] = useState('random title')
 
   const handleClick = () => {
+    if (text === 'random title') {
     setText('hello world');
-  }
+    } else { 
+    setText('random title');
+    }
+  };
   return (
     <React.Fragment>
       <h1>{text}</h1>
