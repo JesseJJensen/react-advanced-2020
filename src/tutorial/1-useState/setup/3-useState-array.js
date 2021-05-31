b0153cc51ab9d56b982cba6aabd6fc99aa36829e
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-31 12:16:33 -0400

    useState array=>added clear items btn && remove individual item

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { data } from '../../../data'
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState(data)
+  const [people, setPeople] = React.useState(data)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
   return (
     <>
       {people.map((person) => {
@@ -10,9 +15,13 @@ const UseStateArray = () => {
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         )
       })}
+      <button className='btn' onClick={() => setPeople([])}>
+        clear items
+      </button>
     </>
   )
 }
