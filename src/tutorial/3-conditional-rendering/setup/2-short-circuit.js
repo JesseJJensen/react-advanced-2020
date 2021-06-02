8523d57e73b58d1ef40a56851dc6078f46cad747
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-02 08:15:39 -0400

    conditional rendering=>added btn=>click Error...click again no error

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('peter')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
-  console.log(secondValue)
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
 
   return (
     <>
@@ -14,8 +14,10 @@ const ShortCircuit = () => {
       <h1>value: {secondValue}</h1> */}
       {/* {if(){console.log('hello world')}} */}
       <h1>{text || 'john doe'}</h1>
-      {/* {text && <h1>hello world</h1>} */}
-      {!text && <h1>hello world</h1>}
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
     </>
   )
 }
