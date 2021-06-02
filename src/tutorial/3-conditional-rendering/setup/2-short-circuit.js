8862c3d1920458018fff08b3d711a45fb30c6923
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-02 08:08:18 -0400

    using && statement

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState('peter')
   const firstValue = text || 'hello world'
   const secondValue = text && 'hello world'
   console.log(secondValue)
@@ -14,6 +14,8 @@ const ShortCircuit = () => {
       <h1>value: {secondValue}</h1> */}
       {/* {if(){console.log('hello world')}} */}
       <h1>{text || 'john doe'}</h1>
+      {/* {text && <h1>hello world</h1>} */}
+      {!text && <h1>hello world</h1>}
     </>
   )
 }
