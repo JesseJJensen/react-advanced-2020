4ba4781bc8d9bcaa9d6eac35b8179feabe461b1b
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-03 10:22:51 -0400

    forms controlled inputs =>dont actually need name: and email:

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -12,7 +12,7 @@ const ControlledInputs = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (firstName && email) {
-      const person = { firstName: firstName, email: email }
+      const person = { firstName, email }
       console.log(person)
     } else {
       console.log('empty values')
