5503b630c71fea9175311d535e867daa5f040feb
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-03 11:38:15 -0400

    forms controlled inputs => added id: toString also iterating over w/ map and displaying on screen

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,11 +9,18 @@ const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
   const [people, setPeople] = useState([])
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (firstName && email) {
-      const person = { firstName, email }
+      const person = { id: new Date().getTime().toString(), firstName, email }
       console.log(person)
+      setPeople((people) => {
+        return [...people, person]
+      })
+      setFirstName('')
+      setEmail('')
+      console.log(firstName, email)
     } else {
       console.log('empty values')
     }
@@ -46,6 +53,15 @@ const ControlledInputs = () => {
             add person
           </button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person
+          return (
+            <div className='item' key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          )
+        })}
       </article>
     </>
   )
