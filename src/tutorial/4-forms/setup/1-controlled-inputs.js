5554f126f48cd0ba3b3b17041a96633399663260
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-03 10:20:34 -0400

    forms controlled input => created array, now people will get added to array

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,9 +8,15 @@ import React, { useState } from 'react'
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(firstName, email)
+    if (firstName && email) {
+      const person = { firstName: firstName, email: email }
+      console.log(person)
+    } else {
+      console.log('empty values')
+    }
   }
   return (
     <>
