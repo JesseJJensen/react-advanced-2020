7a9c8a7672aa5834a79ea324cca8c4fc4c9c4735
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-06-03 13:00:35 -0400

    forms many inputs => updated handleChange and handleSubmit => got rid of useState for name,email,age and added all to [person, setPerson] = useState({name:,email:,age:})

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -10,15 +10,21 @@ const ControlledInputs = () => {
   // const [firstName, setFirstName] = useState('')
   // const [email, setEmail] = useState('')
   // const [age, setAge] = useState('')
-  const [person, SetPerson] = useState({ firstName: '', email: '', age: '' })
+  const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
   const [people, setPeople] = useState([])
 
   const handleChange = (e) => {
     const name = e.target.name
     const value = e.target.value
+    setPerson({ ...person, [name]: value })
   }
   const handleSubmit = (e) => {
     e.preventDefault()
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() }
+      setPeople([...people, newPerson])
+      setPerson({ firstName: '', email: '', age: '' })
+    }
   }
 
   return (
